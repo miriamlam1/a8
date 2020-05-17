e601196641205d635898f51b6ff331827f9fde57
mlam27 <mlam27@calpoly.edu>
2020-05-17 12:12:51 -0700

    jsdhf

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ void main(void)
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 	set_DCO(FREQ_24_MHz);
 	UART_init();
+	DAC_init();
 
 	char data = 0;
 	uint16_t final_num = 0;
@@ -30,21 +31,29 @@ void main(void)
 	while(1){
 	    if(get_receive_flag()){ //Receiving data
 	        data = receive_data();
-	        if(data == ENTER_KEY || counter == 4){
-	            final_num = array_to_int(data_array);//call the function to turn array into int
-	            sendto_DAC(1500);
-	            counter = 0;             //resetting counter
-	            for(i = 0; i < 4; i++){   //resetting array
-	                data_array[i] = '\0';
-	            }
-	        }
-	        else if(data >= '0' &&
-	                data <= '9' &&
-	                data > LOWER_BOUND &&
-	                data < UPPER_BOUND){
+
+	        delay_us(5000);
+	        transmit_data(data);
+	        delay_us(50000);
+
+	        if(data >= '0' &&
+	                data <= '9'
+	            ){
 	            data_array[counter] = data;
 	            counter +=1;
 	        }
+	        if(data == ENTER_KEY || data == 13 || counter >= 4){
+	                        final_num = array_to_int(data_array);//call the function to turn array into int
+	                        if (
+	                        final_num >= LOWER_BOUND &&
+	                        final_num <= UPPER_BOUND){
+	                            sendto_DAC(final_num);
+	                        }
+	                        counter = 0;             //resetting counter
+	                        for(i = 0; i < 4; i++){   //resetting array
+	                            data_array[i] = '\0';
+	                        }
+	                    }
 	        set_receive_flag(0);
 	        EUSCI_A0 -> IE |= EUSCI_A_IE_RXIE;   //enable receive interrupt
 	    }
